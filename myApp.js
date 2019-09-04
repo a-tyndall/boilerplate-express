fe57114ecc41e9033a78db487a74ba48f7b13539
Glitch (a-tyndall-boilerplate-express) <none>
2019-09-04 02:04:51 +0000

    🏗🚖 Checkpoint ./myApp.js:6505928/203

--- myApp.js
+++ myApp.js
@@ -27,7 +27,10 @@ app.use(express.static(__dirname + '/public'));
 
 /** 5) serve JSON on a specific route */
 app.get('/json', (req, res) => {
-  res.json({"message": "Hello json"});
+  let myResponse = {"message": "Hello json"};
+  if (process.env.MESSAGE_STYLE === 'uppercase')
+    myResponse.message = myResponse.message.toUpperCase();
+  res.json(myResponse);
 })
 
 /** 6) Use the .env file to configure the app */
