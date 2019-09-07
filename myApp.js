76d35ba58f4ba0326c5beeb450cc24226715d6fb
Glitch (a-tyndall-boilerplate-express) <none>
2019-09-07 21:42:21 +0000

    👣😄 Checkpoint ./myApp.js:6505928/430

--- myApp.js
+++ myApp.js
@@ -26,6 +26,7 @@ app.use(express.static(__dirname + '/public'));
 
 
 /** 5) serve JSON on a specific route */
+/** 6) Use the .env file to configure the app */
 app.get('/json', (req, res) => {
   let myResponse = {"message": "Hello json"};
   if (process.env.MESSAGE_STYLE === 'uppercase')
@@ -33,7 +34,6 @@ app.get('/json', (req, res) => {
   res.json(myResponse);
 })
 
-/** 6) Use the .env file to configure the app */
  
  
 /** 7) Root-level Middleware - A logger */
